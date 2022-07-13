c84d4e8adf8a85533cd4be4604edc1dd2844c17e
Nico Petzendorfer <nico.petzendorfer@tum.de>
2022-07-13 17:55:12 +0200

    Added mul

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     Pattern plus = Pattern.compile(".*what is (\\d+) plus (\\d+).*");
+    Pattern mul = Pattern.compile(".*what is (\\d+) multiplied by (\\d+).*");
     Pattern max = Pattern.compile(".*which of the following numbers is the largest: ([\\d, ]*)");
 
     public String process(String query) {
         var plusmatcher = plus.matcher(query);
-        var maxmatcher = plus.matcher(query);
+        var maxmatcher = max.matcher(query);
+        var mulmatcher = mul.matcher(query);
         query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
@@ -23,6 +25,8 @@ public class QueryProcessor {
             return query.substring(0, query.indexOf(":"));
         } else if (plusmatcher.matches()) {
             return "" + (Integer.valueOf(plusmatcher.group(1)) + Integer.valueOf(plusmatcher.group(2)));
+        } else if (mulmatcher.matches()) {
+            return "" + (Integer.valueOf(mulmatcher.group(1)) * Integer.valueOf(mulmatcher.group(2)));
         } else if (maxmatcher.matches()) {
             return "" + Arrays.stream(maxmatcher.group(1).split(", ")).mapToInt(Integer::valueOf).max().getAsInt();
         } else { // TODO extend the programm here
